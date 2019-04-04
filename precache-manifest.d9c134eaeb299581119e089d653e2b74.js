cf7e38bbceca5a99adf18ed47598282d2c42446e
Laurie Gloge <laurie@friendlyroad.com>
2019-04-04 14:34:58 -0600

    Updates

---
+++ precache-manifest.d9c134eaeb299581119e089d653e2b74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78557500c5bbe895ba75",
+    "url": "/my-react-app/static/css/main.6d5d52a4.chunk.css"
+  },
+  {
+    "revision": "78557500c5bbe895ba75",
+    "url": "/my-react-app/static/js/main.926ed679.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "d53f0bf5c2b377ceba7b",
+    "url": "/my-react-app/static/js/2.8ccd1fcb.chunk.js"
+  },
+  {
+    "revision": "5b972ea6396d47d660f8d8150b377614",
+    "url": "/my-react-app/index.html"
+  }
+];
