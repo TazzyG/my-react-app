c13b853e16e2e4b035fca9bc3b34d61444bc9154
Laurie Gloge <laurie@friendlyroad.com>
2019-04-10 12:50:52 -0600

    add bootstrap css

---
+++ src/components/banner.jsx
@@ -0,0 +1,18 @@
+<div className="homepage-hero-module">
+  <div className="video-container">
+    <div className="filter" />
+    <video
+      autoplay
+      loop
+      muted
+      playsinline
+      src="PATH_TO_MP4"
+      className="fillWidth"
+    />
+    <div className="poster hidden">
+      <img src="PATH_TO_JPEG" alt="walk in sand">
+        {" "}
+      </img>
+    </div>
+  </div>
+</div>;
