1a1d0da77ce70baeb9f4cc98d9f26628640a4bd5
Laurie Gloge <laurie@friendlyroad.com>
2019-04-10 17:45:26 -0600

    add video but not working yet

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,18 +1,25 @@
-<div className="homepage-hero-module">
-  <div className="video-container">
-    <div className="filter" />
-    <video
-      autoplay
-      loop
-      muted
-      playsinline
-      src="PATH_TO_MP4"
-      className="fillWidth"
-    />
-    <div className="poster hidden">
-      <img src="PATH_TO_JPEG" alt="walk in sand">
-        {" "}
-      </img>
-    </div>
-  </div>
-</div>;
+import React, { Component } from "react";
+import sand from "../banner/sand.jpg";
+// import sand_video from "..banner/sand_video.mp4";
+import "../App.css";
+import "./video.jsx";
+
+class Banner extends Component {
+  render() {
+    return (
+      <div className="homepage-hero-module">
+        <div className="video-container">
+          <div className="filter" />
+          <video autoplay loop muted playsinline className="fillWidth">
+            <source src={"../Videos/sand_video.mp4"} type="video/mp4" />
+          </video>
+          <div className="poster hidden">
+            <img src={sand} alt="walk in sand" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Banner;
