70b51819a906f2e1e1fb56d4473f11d92a5ec7ee
Laurie Gloge <laurie@friendlyroad.com>
2019-04-10 19:57:29 -0600

    update banner with video import

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import sand from "../banner/sand.jpg";
-// import sand_video from "..banner/sand_video.mp4";
+import sand_video from "../banner/sand_video.mp4";
 import "../App.css";
 import "./video.jsx";
 
@@ -10,9 +10,7 @@ class Banner extends Component {
       <div className="homepage-hero-module">
         <div className="video-container">
           <div className="filter" />
-          <video autoplay loop muted playsinline className="fillWidth">
-            <source src={"../Videos/sand_video.mp4"} type="video/mp4" />
-          </video>
+          <video src={sand_video} autoPlay loop muted />
           <div className="poster hidden">
             <img src={sand} alt="walk in sand" />
           </div>
