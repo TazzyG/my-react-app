f777d26d2335e546bd1199420f6305fe024f1ba9
Laurie Gloge <laurie@friendlyroad.com>
2019-12-17 18:13:24 -0700

    Updates

---
+++ precache-manifest.c7b5c7b007468fc4c7dbd5882eede671.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe96f3d9b709688456fea983113db71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f3e4cdafa2348a682f7",
+    "url": "/static/css/2.1fc90354.chunk.css"
+  },
+  {
+    "revision": "8cc463a7835343707f20",
+    "url": "/static/css/main.cf5556db.chunk.css"
+  },
+  {
+    "revision": "2f3e4cdafa2348a682f7",
+    "url": "/static/js/2.4634b1df.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/static/js/2.4634b1df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cc463a7835343707f20",
+    "url": "/static/js/main.47cf9ff9.chunk.js"
+  },
+  {
+    "revision": "22281826f6f5bffce8ea",
+    "url": "/static/js/runtime-main.04e23692.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "62a98265b312efa5bb79822bd23fc38d",
+    "url": "/static/media/john-doyle-1206473-unsplash.62a98265.jpg"
+  },
+  {
+    "revision": "4e215efb91991a371539c57244679191",
+    "url": "/static/media/sand_video.4e215efb.mp4"
+  }
+]);
